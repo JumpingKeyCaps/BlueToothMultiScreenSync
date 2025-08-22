9d7fc2e5aa1216e0257cefb697524308fbba1ea7
Samuel Lebailly <sam.meusa@gmail.com>
2025-08-22 18:43:58 +0200

    feature(main/): Add dynamic permission check (on runtime) for bluetooth feature + tweak activity to call the BT test composable.

--- app/src/main/java/com/lebaillyapp/bluetoothmultiscreensync/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/bluetoothmultiscreensync/MainActivity.kt
@@ -1,26 +1,52 @@
 package com.lebaillyapp.bluetoothmultiscreensync
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lebaillyapp.bluetoothmultiscreensync.ui.screen.BluetoothDemoScreen
 import com.lebaillyapp.bluetoothmultiscreensync.ui.theme.BlueToothMultiScreenSyncTheme
+import androidx.core.app.ActivityCompat
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Request Bluetooth permissions dynamically
+        requestBtPermissions()
+
         setContent {
             BlueToothMultiScreenSyncTheme {
-               //to done !
+                Column(modifier = Modifier.fillMaxSize().padding(top = 36.dp)) {
+                    // Test BT feature !
+                    BluetoothDemoScreen()
+                }
             }
         }
+    }
+
+    private fun requestBtPermissions() {
+        val perms = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            arrayOf(
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.BLUETOOTH_SCAN
+            )
+        } else {
+            arrayOf(
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_ADMIN
+            )
+        }
+
+        ActivityCompat.requestPermissions(this, perms, 0)
     }
 }
