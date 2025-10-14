355ee97582748d3d748a0a1406f6b2364906e65a
Samuel Lebailly <sam.meusa@gmail.com>
2025-10-14 18:00:22 +0200

    feature(domain/model/virtualPlane): set the enum Serializable.

--- app/src/main/java/com/lebaillyapp/bluetoothmultiscreensync/domain/model/virtualPlane/VirtualOrientation.kt
+++ app/src/main/java/com/lebaillyapp/bluetoothmultiscreensync/domain/model/virtualPlane/VirtualOrientation.kt
@@ -1,10 +1,13 @@
 package com.lebaillyapp.bluetoothmultiscreensync.domain.model.virtualPlane
 
+import kotlinx.serialization.Serializable
+
 /**
  * Enum representing the virtual orientation of a viewport.
  * Allows the master device to define a logical orientation
  * that may differ from the physical orientation of the device.
  */
+@Serializable
 enum class VirtualOrientation {
     NORMAL, ROTATED_90, ROTATED_180, ROTATED_270, FLIPPED_X, FLIPPED_Y
 }
